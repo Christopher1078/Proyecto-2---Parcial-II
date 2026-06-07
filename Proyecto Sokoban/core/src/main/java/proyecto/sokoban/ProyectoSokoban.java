91a48059069cf0302d383659cf7d4c65bdc8c509
Christopher1078 <christortizfun@gmail.com>
2026-06-07 15:13:40 -0600

    LibGDX

---
+++ Proyecto Sokoban/core/src/main/java/proyecto/sokoban/ProyectoSokoban.java
@@ -0,0 +1,11 @@
+package proyecto.sokoban;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class ProyectoSokoban extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
