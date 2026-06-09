446f3f23ca1aa3ee315e7f4aa651d90f3dc93fb2
Christopher1078 <christortizfun@gmail.com>
2026-06-08 22:22:03 -0600

    LogIn Screen

--- Proyecto Sokoban/core/src/main/java/proyecto/sokoban/ProyectoSokoban.java
+++ Proyecto Sokoban/core/src/main/java/proyecto/sokoban/ProyectoSokoban.java
@@ -6,6 +6,6 @@ import com.badlogic.gdx.Game;
 public class ProyectoSokoban extends Game {
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        setScreen(new LoginScreen(this));
     }
 }
